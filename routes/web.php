cee1fe0ccd51b96cf315d149451e6633d7254d7f
Dhairya <dhairya00798@gmail.com>
2020-11-18 12:38:30 +0530

    user logging done

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Auth\RegisterController;
 
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
-// 'index' is method(function) name
 Route::post('/register', [RegisterController::class, 'store']);
 // 'store' is method(function) name
 
